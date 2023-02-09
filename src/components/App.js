54ee61b2eeec71bd01005a31a662c86e03d057c5
VannakDim <hannah@Vannaks-MacBook-Pro.local>
2023-02-09 23:19:57 +0700

    add footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import logo from '../logo.svg';
 import Navbar from './partial/navbar';
-import Home from './home';
+import Home from '../pages/home';
 import Carousel from './partial/carousel';
 import Foot from './footer';
 import pic from '../img/bsea-logo.png';
